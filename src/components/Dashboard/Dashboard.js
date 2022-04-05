49eb3f956ed72a29ba552545d1a1a44995ef5dd4
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2022-04-05 18:47:02 +0200

    dashboard completed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Dashboard.css";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/product-analysis-website/main/data.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <div>
-      <h1>This is Dashboard</h1>
+      <div className="chart-container">
+        <div className="bar-chart">
+          <h2>BAR CHART</h2>
+          <BarChart width={500} height={250} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="investment" fill="#FF0000" />
+            <Bar dataKey="revenue" fill="#000000" />
+          </BarChart>
+        </div>
+        <div className="line-chart">
+          <h2>LINE CHART</h2>
+          <LineChart
+            width={500}
+            height={250}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="investment" stroke="#FF0000" />
+            <Line type="monotone" dataKey="revenue" stroke="#000000" />
+          </LineChart>
+        </div>
+      </div>
     </div>
   );
 };
